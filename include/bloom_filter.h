31eeae4eea106c8584a99329c69ef0db83462e57
YincongLyu <yinconglyu@foxmail.com>
2024-05-26 23:48:38 +0800

    refactor: bloom filter chain and iter inner type

--- include/bloom_filter.h
+++ include/bloom_filter.h
@@ -32,28 +32,31 @@ private:
  */
 class BloomFilterChain {
 public:
-    BloomFilterChain(uint32_t slot_num, uint8_t nfunc);
+    BloomFilterChain(uint32_t slot_num, uint8_t nfunc, size_t align);
     void join(BloomFilter&& new_filter, size_t new_block_addr);
     ChainIterator* test(std::span<uint8_t> key);
 
 
 private:
-    std::vector<size_t> block_addr_;
-    std::vector<uint64_t> matrix_;
+    std::vector<uint64_t> space;
+    std::span<size_t> block_addr_;
     size_t chain_len_;
+
+    std::span<uint64_t> matrix_; /* 整合多个 slots */
     uint8_t nfunc_;
 };
 
 class ChainIterator {
 public:
-    ChainIterator(std::vector<size_t>& block_addr, uint64_t bitmask, uint8_t progress);
+    ChainIterator(std::span<size_t> block_addr, uint64_t bitmask, uint8_t progress);
     void next(size_t& addr, bool& is_depleted);
 
 
 private:
-    std::vector<size_t>& block_addr_;
+    std::span<size_t> block_addr_;
+    uint8_t progress_; /* 用来判断是否迭代完了 block_addr_, 从0开始*/
     uint64_t bitmask_;
-    uint8_t progress_;
+
     friend BloomFilterChain;
 };
 
--- src/bloom_filter.cc
+++ src/bloom_filter.cc
@@ -39,7 +39,7 @@ bool BloomFilter::test(std::span<uint8_t> key) {
  * @param bitmask 用来判断当前的 key 是否在 bloom chain里
  * @param progress 当前迭代的进度
  */
-ChainIterator::ChainIterator(std::vector<size_t>& block_addr, 
+ChainIterator::ChainIterator(std::span<size_t> block_addr, 
     uint64_t bitmask, uint8_t progress) :
 block_addr_(block_addr), bitmask_(bitmask), progress_(progress) {}
 
@@ -50,12 +50,15 @@ block_addr_(block_addr), bitmask_(bitmask), progress_(progress) {}
  * @param is_depleted 判断是否还可以再继续迭代
  */
 void ChainIterator::next(size_t& addr, bool& is_depleted) {
+    // 遍历完了所有 bloom filter，也对应所有的 block
     if (this->progress_ == block_addr_.size()) {
         is_depleted = true;
         return;
     }
-    if ((this->bitmask_ >> this->progress_) & 1) {
-        addr = this->block_addr_[this->block_addr_.size() - this->progress_ - 1];
+    // 检查当前 bitmask_ 的progress进度
+    auto check_pos = this->block_addr_.size() - this->progress_ - 1;
+    if ((this->bitmask_) & (uint64_t{1} << check_pos)) {
+        addr = this->block_addr_[this->progress_];
         is_depleted = false;
         this->progress_ += 1;
         return;
@@ -65,9 +68,8 @@ void ChainIterator::next(size_t& addr, bool& is_depleted) {
     }
 }
 
-BloomFilterChain::BloomFilterChain(uint32_t slot_num, uint8_t nfunc) : 
-    block_addr_(64, 0),
-    matrix_(slot_num, 0),
+BloomFilterChain::BloomFilterChain(uint32_t slot_num, uint8_t nfunc, size_t align) : 
+    space(0),
     chain_len_(0),
     nfunc_(nfunc)
 {}
@@ -84,20 +86,21 @@ void BloomFilterChain::join(BloomFilter&& new_filter, size_t new_block_addr) {
             this->matrix_[i] |= (1 << (63 - this->chain_len_));
         }
     }
-    this->block_addr_.emplace_back(new_block_addr);
+    this->block_addr_[this->chain_len_] = new_block_addr;
     this->chain_len_ += 1;
 }
 
 ChainIterator* BloomFilterChain::test(std::span<uint8_t> key) {
     uint32_t hash_a = hash(key, static_cast<uint32_t>('l'));
     uint32_t hash_b = hash(key, static_cast<uint32_t>('y'));
-    bool exist = true;
+    // before : bool exist = true; 现在用 uint64_t 类型 表示 真值
+    uint64_t exist = ~uint64_t{0};
     for (size_t i = 0; i < nfunc_; i++) {
         uint32_t hash_z = mangle(i, hash_a, hash_b, matrix_.size());
-        // 注意：这里和单个 bloom filter检测一个key是否存在的区别
+        // 注意：这里和单个 bloom filter检测一个key是否存在的区别，一个key可以同时在多个bloomfilter里检查是否存在
         exist &= this->matrix_[i];
     }
-    return new ChainIterator({this->block_addr_, exist, 0});
+    return new ChainIterator(this->block_addr_, exist, 0);
 }
 
 }
